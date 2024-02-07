663eee2b02f71500b9754cdf34c57cfc6fb76065
BuildTools <unconfigured@null.spigotmc.org>
2024-02-07 21:41:12 +1000

    added the core client command and create a version sub-command node

--- src/main/java/net/zedbear/skyblocknucleus/SkyblockNucleus.java
+++ src/main/java/net/zedbear/skyblocknucleus/SkyblockNucleus.java
@@ -1,16 +1,16 @@
 package net.zedbear.skyblocknucleus;
 
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SkyblockNucleus implements ModInitializer {
 	public static final String MOD_ID = "skyblocknucleus";
+	public static final String MOD_VERSION = "BETA-0.1 Minecraft 1.20.4";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+
 	}
 }
--- src/main/java/net/zedbear/skyblocknucleus/SkyblockNucleusClient.java
+++ src/main/java/net/zedbear/skyblocknucleus/SkyblockNucleusClient.java
@@ -1,10 +1,26 @@
 package net.zedbear.skyblocknucleus;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.minecraft.text.Text;
+import static net.minecraft.server.command.CommandManager.*;
+import static net.zedbear.skyblocknucleus.SkyblockNucleus.MOD_VERSION;
 
 public class SkyblockNucleusClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> dispatcher.register(ClientCommandManager.literal("sbn")
+                .then(ClientCommandManager.literal("version")
+                        .executes(context -> {
+                            context.getSource().sendFeedback(Text.of("SBN Version " + MOD_VERSION));
+                            return 1;
+                        })
+                )
+                .executes(context -> {
+                    context.getSource().sendFeedback(Text.of("Missing arguments!"));
+                    return 1;
+                })
+        ));
     }
 }
